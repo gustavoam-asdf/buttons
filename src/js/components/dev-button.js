ed9fa05359fcb018668f31a57dd9f78e27d178fc
Gustavo A <atencio361@hotmail.com>
2021-07-27 18:12:52 -0500

    Using attachShadow closed mode, add variant text and using selectors for button

--- src/js/components/dev-button.js
+++ src/js/components/dev-button.js
@@ -1,40 +1,40 @@
-class DevButton extends HTMLElement {
-  constructor() {
-    super()
-    this.attachShadow({
-      mode: 'open',
-      delegatesFocus: true
-    })
-    this.styleSheet = document.createElement('link')
-    this.styleSheet.setAttribute('rel', 'stylesheet')
-    this.styleSheet.setAttribute('href', '/src/css/components/button.css')
+{
+  let root
+  class DevButton extends HTMLElement {
+    constructor() {
+      super()
+      root = this.attachShadow({
+        mode: 'closed'
+      })
+      this.styleSheet = document.createElement('link')
+      this.styleSheet.setAttribute('rel', 'stylesheet')
+      this.styleSheet.setAttribute('href', '/src/css/components/button.css')
 
-    this.button = document.createElement('button')
-    this.button.classList.add('button')
-  }
+      this.button = document.createElement('button')
+    }
 
-  connectedCallback() {
-    this.shadowRoot.prepend(this.styleSheet)
-    this.shadowRoot.append(this.button)
-  }
+    connectedCallback() {
+      root.prepend(this.styleSheet)
+      root.append(this.button)
+    }
 
-  static get observedAttributes() {
-    return ['text', 'box-shadow', 'variant']
-  }
+    static get observedAttributes() {
+      return ['text', 'box-shadow', 'variant']
+    }
 
-  attributeChangedCallback(attr, oldValue, newValue) {
-    console.log({ attr, oldValue, newValue })
-    if (attr === 'text') {
-      this.button.textContent = newValue
-    } else if (attr === 'box-shadow') {
-      newValue === ''
-        ? this.button.classList.add('box-shadow')
-        : this.button.classList.remove('box-shadow')
-    } else if (attr === 'variant') {
-      if (newValue === 'button') return
-      this.button.classList.remove(oldValue)
-      this.button.classList.add(newValue)
+    attributeChangedCallback(attr, oldValue, newValue) {
+      console.log({ attr, oldValue, newValue })
+      if (attr === 'text') {
+        this.button.textContent = newValue
+      } else if (attr === 'box-shadow') {
+        newValue === ''
+          ? this.button.classList.add('box-shadow')
+          : this.button.classList.remove('box-shadow')
+      } else if (attr === 'variant') {
+        this.button.classList.remove(oldValue)
+        this.button.classList.add(newValue)
+      }
     }
   }
+  customElements.define('dev-button', DevButton)
 }
-customElements.define('dev-button', DevButton)
