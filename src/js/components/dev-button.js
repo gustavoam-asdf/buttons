794957d3abb22805b82d86a12ff078400e502792
Gustavo A <atencio361@hotmail.com>
2021-07-27 22:28:11 -0500

    Buttons size and disabled buttons

--- src/js/components/dev-button.js
+++ src/js/components/dev-button.js
@@ -8,31 +8,38 @@
       })
       this.styleSheet = document.createElement('link')
       this.styleSheet.setAttribute('rel', 'stylesheet')
-      this.styleSheet.setAttribute('href', '/src/css/components/button.css')
+      this.styleSheet.setAttribute('href', './css/components/button.css')
 
       this.button = document.createElement('button')
+      this.initialHTML = document.createElement('div')
+      this.initialHTML.insertAdjacentHTML('afterbegin', this.innerHTML)
     }
 
     connectedCallback() {
+      this.button.textContent = this.initialHTML.textContent
       root.prepend(this.styleSheet)
       root.append(this.button)
+      this.innerHTML = ''
     }
 
     static get observedAttributes() {
-      return ['text', 'box-shadow', 'variant']
+      return ['box-shadow', 'variant', 'size', 'disabled']
     }
 
     attributeChangedCallback(attr, oldValue, newValue) {
       console.log({ attr, oldValue, newValue })
-      if (attr === 'text') {
-        this.button.textContent = newValue
-      } else if (attr === 'box-shadow') {
+      if (attr === 'box-shadow') {
         newValue === ''
           ? this.button.classList.add('box-shadow')
           : this.button.classList.remove('box-shadow')
       } else if (attr === 'variant') {
-        this.button.classList.remove(oldValue)
-        this.button.classList.add(newValue)
+        this.button.classList.remove(`variant-${oldValue}`)
+        this.button.classList.add(`variant-${newValue}`)
+      } else if (attr === 'size') {
+        this.button.classList.remove(`size-${oldValue}`)
+        this.button.classList.add(`size-${newValue}`)
+      } else if (attr === 'disabled') {
+        this.button.setAttribute(attr, newValue)
       }
     }
   }
