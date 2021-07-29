1c58cc485b5719738a5b937540d2793f9f66ac43
Gustavo A <atencio361@hotmail.com>
2021-07-29 11:23:18 -0500

    Adding icons and download fontawesome

--- src/js/components/dev-button.js
+++ src/js/components/dev-button.js
@@ -6,28 +6,29 @@
       root = this.attachShadow({
         mode: 'closed'
       })
+      // StyleSheet
       this.styleSheet = document.createElement('link')
       this.styleSheet.setAttribute('rel', 'stylesheet')
       this.styleSheet.setAttribute('href', './css/components/button.css')
 
       this.button = document.createElement('button')
-      this.initialHTML = document.createElement('div')
-      this.initialHTML.insertAdjacentHTML('afterbegin', this.innerHTML)
+      this.button.innerHTML = `
+        <slot name="left-icon" class="icon fas" ></slot>
+        <slot name="text"></slot>
+        <slot name="right-icon" class="icon fas"></slot>
+      `
     }
 
     connectedCallback() {
-      this.button.textContent = this.initialHTML.textContent
       root.prepend(this.styleSheet)
       root.append(this.button)
-      this.innerHTML = ''
     }
 
     static get observedAttributes() {
-      return ['box-shadow', 'variant', 'size', 'disabled', 'color']
+      return ['box-shadow', 'variant', 'size', 'color', 'disabled']
     }
 
     attributeChangedCallback(attr, oldValue, newValue) {
-      console.log({ attr, oldValue, newValue })
       if (attr === 'box-shadow') {
         newValue === ''
           ? this.button.classList.add('box-shadow')
